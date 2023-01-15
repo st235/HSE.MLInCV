569093415802bfc86a29fff0780a6f35604f9f0d
Alexander Dadukin <hi@st235.xyz>
2023-01-15 19:19:51 +0000

    fix demo: show results in interactive mode instead of writing them to the disc.

--- src/demo.py
+++ src/demo.py
@@ -7,46 +7,43 @@ from utils.args_parser import create_parser_for_cli, FLAG_NO_FLAG
 
 
 def process_image(image_file: str):
-    img = cv2.imread(image_file)
-    h = img.shape[0]
-    w = img.shape[1]
+    original_image = cv2.imread(image_file)
+    height = original_image.shape[0]
+    width = original_image.shape[1]
 
-    img = cv2.resize(img, (256, 144))
-    img = np.asarray(img)
-    img = img / 255.
-    img = img.astype(np.float32)
-    img = img[np.newaxis, :, :, :]
-
-    # Tensorflow Lite
+    image = cv2.resize(original_image, (256, 144))
+    image = np.asarray(image)
+    image = image / 255.
+    image = image.astype(np.float32)
+    image = image[np.newaxis, :, :, :]
 
     interpreter = Interpreter(model_path=get_tensorflow_model_file(), num_threads=4)
     interpreter.allocate_tensors()
     input_details = interpreter.get_input_details()[0]['index']
     output_details = interpreter.get_output_details()[0]['index']
 
-    interpreter.set_tensor(input_details, img)
+    interpreter.set_tensor(input_details, image)
     interpreter.invoke()
     output = interpreter.get_tensor(output_details)
 
-    print(output.shape)
-    out1 = output[0][:, :, 0]
-    out2 = output[0][:, :, 1]
+    foreground = output[0][:, :, 0]
+    background = output[0][:, :, 1]
 
-    out1 = np.invert((out1 > 0.5) * 255)
-    out2 = np.invert((out2 > 0.5) * 255)
+    foreground_inverted = np.invert((foreground > 0.5) * 255)
+    background_inverted = np.invert((background > 0.5) * 255)
 
-    print('out1:', out1.shape)
-    print('out2:', out2.shape)
+    foreground_resized = cv2.resize(np.uint8(foreground_inverted), (width, height))
+    background_resized = cv2.resize(np.uint8(background_inverted), (width, height))
 
-    out1 = cv2.resize(np.uint8(out1), (w, h))
-    out2 = cv2.resize(np.uint8(out2), (w, h))
+    final_mask = cv2.ximgproc.jointBilateralFilter(background_resized, foreground_resized, 8, 75, 75)
+    final_mask = cv2.cvtColor(final_mask, cv2.COLOR_GRAY2RGB)
 
-    cv2.imwrite('out1.jpg', out1)
-    cv2.imwrite('out2.jpg', out2)
+    masked_foreground = cv2.addWeighted(original_image, 0.9, final_mask, 0.4, 0)
 
-    out3 = cv2.ximgproc.jointBilateralFilter(out2, out1, 8, 75, 75)
+    cv2.imshow(image_file, masked_foreground)
+    cv2.waitKey(0)
 
-    cv2.imwrite('out3.jpg', out3)
+    cv2.destroyAllWindows()
 
 
 def main():
